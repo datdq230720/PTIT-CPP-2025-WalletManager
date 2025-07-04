0d0e761b111f684fd6a18f2f43840beee58edd2f
Đinh Quốc Đạt <125462317+datdq230720@users.noreply.github.com>
2025-07-04 22:09:26 +0700

    Update OTPManager.h

--- OTPManager.h
+++ OTPManager.h
@@ -11,11 +11,11 @@ private:
     time_t expire_time;
 
 public:
-    OTPManager() : otp_code(""), expire_time(0) {}
+    OTPManager() : otp_code(""), expire_time(0) {}                 
 
     void generate_otp() {
-        otp_code = generate_random_string(6);
-        expire_time = std::time(nullptr) + 60; // OTP sống 60 giây
+        otp_code = generate_random_string(6);               
+        expire_time = std::time(nullptr) + 60;              
     }
 
     std::string get_otp() const {
@@ -25,7 +25,7 @@ public:
     bool validate_otp(const std::string& input) {
         time_t now = std::time(nullptr);
         if (now > expire_time) {
-            return false; // Hết hạn
+            return false; 
         }
         return input == otp_code;
     }
